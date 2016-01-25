9df9a63eed205dcd4568fe41ae1c499cff554ddd
Alex Zhou <AlexZhou@wl-10-190-87-46.wireless.duke.edu>
2016-01-25 11:48:55 -0500

    Initial Commit

---
+++ One Challenge a DayTests/One_Challenge_a_DayTests.swift
@@ -0,0 +1,36 @@
+//
+//  One_Challenge_a_DayTests.swift
+//  One Challenge a DayTests
+//
+//  Created by Alex Zhou on 1/25/16.
+//  Copyright © 2016 Alex Zhou. All rights reserved.
+//
+
+import XCTest
+@testable import One_Challenge_a_Day
+
+class One_Challenge_a_DayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
